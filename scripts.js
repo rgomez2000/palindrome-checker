d58bb9dea2daf505ab84a9a3ae30931f74db34ff
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-08-05 18:21:34 -0700

    Added functionality
    Added functionality to the palindrome checker through JS.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+
+
+checkButton.addEventListener("click",() => {
+  const replaced = textInput.value.toLowerCase().replace(/[^A-Za-z0-9]/g, ""); 
+
+  if (textInput.value === "") {
+    alert("Please input a value");
+  } else if (textInput.value.length === 1) {
+    result.innerText = `${textInput.value} is a palindrome.` 
+  } else if (textInput.value === [...textInput.value].reverse().join("")) {
+    result.innerText = `${textInput.value} is a palindrome.`
+  } else if (replaced === [...replaced].reverse().join("")) {
+    result.innerText = `${textInput.value} is a palindrome.`
+  } else {
+    result.innerText = `${textInput.value} is not a palindrome.`
+  }
+});
+
